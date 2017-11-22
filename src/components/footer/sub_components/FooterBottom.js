bb31676ca18c58f9e7fbe1b0e7f42cab89aeec4f
Antonio Brandao <antoniobrandaodesign@gmail.com>
2017-11-22 06:55:01 +0000

    Style footer bottom

--- src/components/footer/sub_components/FooterBottom.js
+++ src/components/footer/sub_components/FooterBottom.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react'
 
+import icon_rss from '../../../assets/svg/icon_rss_small.svg';
+import icon_mail from '../../../assets/svg/icon_mail_small.svg';
+
 class FooterNavColumns extends Component {
   render() {
     return (
       <div className='footer-bottom'>
         <div className='central-column'>
-          <p>Footer Bottom</p>
+          <div className='content'>
+            <p>Copyright © 2009-2015 Trinity</p>
+            <p>|</p>
+            <p> Blog</p>
+            <img className='rss' src={icon_rss} alt="RSS" />
+            <p>|</p>
+            <p>Newsletter</p>
+            <img className='email' src={icon_mail} alt="Email" />
+          </div>
         </div>
       </div>
     );
